27fb0d1c25ca26353ddc8a29226e5c41a74c2eac
Anastasiya <nastjakoka@mail.ru>
2022-06-30 16:03:06 +0300

    задача 4

--- DZ_1/Program.cs
+++ DZ_1/Program.cs
@@ -75,3 +75,25 @@ else
     Console.WriteLine(num + " нечетное");
 }
 */
+
+/* Задача 4: 
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+
+if (N < 2)
+{
+    Console.Write("Введите число не меньше 2: ");
+}
+
+while (current<=N)
+{
+    Console.Write(current+ " ");
+    current = current+2;
+}
