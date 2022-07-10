ab53993adb682a5905558e65894f65ab1aaabf20
«Anastasiya» <nastjakoka@mail.ru>
2022-07-10 15:57:58 +0300

    4-я лекция конспект

--- Lecture/Lecture_4/Program.cs
+++ Lecture/Lecture_4/Program.cs
@@ -223,6 +223,7 @@ for (int i = 1; i < 40; i++)
 // f(2) = 1
 // f(n) = f(n-1) + f(n-2)
 */
+/*
 int Fibonacci(int n)
 {
     if(n == 1 || n == 2) return 1;
@@ -233,7 +234,7 @@ for (int i = 1; i < 10; i++)
 {
     Console.WriteLine(Fibonacci(i));
 }
-
+*/
 
 
 
