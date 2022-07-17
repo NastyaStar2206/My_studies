63b1118e0b6b75afafa32898a204377f74bd7a2b
«Anastasiya» <nastjakoka@mail.ru>
2022-07-17 15:59:16 +0300

    dz_6

--- DZ_6/Program.cs
+++ DZ_6/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+/*
+ volume = 10;                // количество ввода чисел
+int count = 0;
+
+while (volume > 0)
+{
+    Console.Write("Введите число:   ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) count++;
+    volume--;
+}
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
+*/
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
