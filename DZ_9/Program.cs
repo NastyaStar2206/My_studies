70c8ef7947bc26584dc34362de4f990c7654ee7f
«Anastasiya» <nastjakoka@mail.ru>
2022-07-25 12:25:42 +0300

    решение DZ_9

--- DZ_9/Program.cs
+++ DZ_9/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+// N = 4532 -> 4
+/*
+int DigitsCount(int N)
+{
+    if(N>9)
+    {
+        
+        return 1 + DigitsCount(N/10);
+    }
+   else
+    return 1;
+}
+Console.WriteLine(DigitsCount(230043));
+*/
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30.
+
+/*
+int SumOfRange(int M, int N)
+{
+    if(M<N)
+    {
+        return M + SumOfRange(M+1, N);
+    }
+    else
+        return M;
+}   
+
+Console.WriteLine(SumOfRange(3, 8));
+*/
